eebd30ac45d9e002d3be7669e5c8782a4468b923
Lindimphilo WJ <66278795+Libertalia-estate-agency@users.noreply.github.com>
2025-03-14 00:59:52 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Increase payload limit to handle large images
 app.use(express.json({ limit: "100mb" }));
-app.use(cors());
+app.use(cors({ origin :'true' }));
 app.use(bodyParser.json());
 
 
