18a43a39dabe32620155251c4244968fc80e805a
Lindimphilo WJ <66278795+Libertalia-estate-agency@users.noreply.github.com>
2025-03-14 02:35:50 +0200

    required

--- api/index.js
+++ api/index.js
@@ -3,12 +3,17 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const sharp = require("sharp");
-const compression = require("compression"); // Import compression
+//const compression = require("compression"); // Import compression
+const shrinkRay = require("shrink-ray-current"); // Brotli & Gzip compression
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(compression()); // Enable Gzip compression
+//app.use(compression()); // Enable Gzip compression
+
+
+app.use(shrinkRay()); // Enable Brotli & Gzip compression
 
 // Increase payload limit to handle large images
 app.use(express.json({ limit: "100mb" }));
